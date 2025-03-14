932308fd425f60bfb64e7461e816e43e6c00d95c
ToniOrtizz <antoniortiz2006@ginebro.cat>
2025-03-14 17:17:06 +0100

    commit 17:16

---
+++ ortiz-m0485-20250314/ortiz-grup-b-primer-enunciat.py
@@ -0,0 +1,10 @@
+if __name__ == '__main__':
+
+    posicion = int()
+    vNomsPersonatges = int()
+    vClassesPersonatges = list()
+    vNivellsPoderPersonatges = list()
+
+vNomsPersonatges = ["gener","febrer","març","abril",
+                 "maig","juny","juliol","agost",
+                 "setembre","octubre","novembre","desembre"]
