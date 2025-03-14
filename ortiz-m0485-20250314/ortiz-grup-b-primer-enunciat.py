fe0a8884361ec3585cd1bec12a0248a06e601aa1
ToniOrtizz <antoniortiz2006@ginebro.cat>
2025-03-14 17:39:03 +0100

    commit 17:40

--- ortiz-m0485-20250314/ortiz-grup-b-primer-enunciat.py
+++ ortiz-m0485-20250314/ortiz-grup-b-primer-enunciat.py
@@ -1,10 +1,47 @@
-if __name__ == '__main__':
+vNomsPersonatges = []
+vClassesPersonatges = []
+vNivellsPoderPersonatges = []
 
-    posicion = int()
-    vNomsPersonatges = int()
-    vClassesPersonatges = list()
-    vNivellsPoderPersonatges = list()
 
-vNomsPersonatges = ["gener","febrer","març","abril",
-                 "maig","juny","juliol","agost",
-                 "setembre","octubre","novembre","desembre"]
+def afegirPersonatge(nomRebut, classeRebuda, poderRebut):
+
+    if poderRebut < 0:
+        print("ERROR: el nivell de poder es incorrectra ja q no pot ser negatiu.")
+        return
+    vNomsPersonatges.append(nomRebut)
+    vClassesPersonatges.append(classeRebuda)
+    vNivellsPoderPersonatges.append(poderRebut)
+
+    print(f"personatje {nomRebut} registrat bee!")
+def determinarRangPersonatge(poder):
+
+    if poder <= 100:
+        return "NOVATO"
+    elif poder <= 250:
+        return "EXPERT"
+    elif poder <= 400:
+        return "MAESTRO"
+    else:
+        return "Llegenda"
+def mostrarPersonatges():
+
+    if not vNomsPersonatges:
+        print("No hi ha personatges registrats.")
+        return
+
+    print("\nLlista de personatges:")
+    for i in range(len(vNomsPersonatges)):
+        nom = vNomsPersonatges[i]
+        classe = vClassesPersonatges[i]
+        poder = vNivellsPoderPersonatges[i]
+        rang = determinarRangPersonatge(poder)
+        print(f"Nom: {nom}, Classe: {classe}, Poder: {poder}, Rang: {rang}")
+
+
+if __name__ == "__main__":
+
+
+
+
+
+
